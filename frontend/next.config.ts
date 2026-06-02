15bea710b78847dcaa63440560212f40a87b6726
Akingbesote Babajide <akingbesotebabajide@gmail.com>
2026-06-02 21:06:15 +0100

    fix: rpy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,11 +3,31 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { hostname: 'image.tmdb.org' },
-      { hostname: 'i.ytimg.com' },
-      { hostname: 'img.youtube.com' },
-      { hostname: 'www.themoviedb.org' },
-      { hostname: 'images.unsplash.com' },
+      // TMDB
+      { protocol: 'https', hostname: 'image.tmdb.org', pathname: '/t/p/**' },
+      { protocol: 'https', hostname: 'www.themoviedb.org' },
+
+      // YouTube thumbnails
+      { protocol: 'https', hostname: 'i.ytimg.com' },
+      { protocol: 'https', hostname: 'img.youtube.com' },
+
+      // Unsplash
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+
+      // TheNkiri (WordPress)
+      { protocol: 'https', hostname: 'thenkiri.com' },
+      { protocol: 'https', hostname: '**.thenkiri.com' },
+
+      // DLDownload (WordPress)
+      { protocol: 'https', hostname: 'dldownload.com.ng' },
+      { protocol: 'https', hostname: '**.dldownload.com.ng' },
+
+      // WordPress CDN (used by both sources)
+      { protocol: 'https', hostname: '**.wordpress.com' },
+      { protocol: 'https', hostname: '**.wp.com' },
+      { protocol: 'https', hostname: 'i0.wp.com' },
+      { protocol: 'https', hostname: 'i1.wp.com' },
+      { protocol: 'https', hostname: 'i2.wp.com' },
     ],
   },
 };
