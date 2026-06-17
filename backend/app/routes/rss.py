9e7b49188e41661c372362bf89d2a82ffeff0357
Akingbesote Babajide <akingbesotebabajide@gmail.com>
2026-06-17 14:09:20 +0100

    commit new rss up

--- backend/app/routes/rss.py
+++ backend/app/routes/rss.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, make_response
 from feedgen.feed import FeedGenerator
 from app.models.movie import Movie
-from datetime import timezone
+from app.models.series import Series
+from datetime import timezone, datetime
 
 rss_bp = Blueprint('rss', __name__, url_prefix='/api')
 
@@ -16,21 +17,38 @@ def rss_feed():
     fg.language('en')
 
     movies = Movie.query.order_by(Movie.created_at.desc()).limit(50).all()
+    series = Series.query.order_by(Series.created_at.desc()).limit(50).all()
 
-    for movie in movies:
+    items = [('movie', m) for m in movies] + [('series', s) for s in series]
+
+    items.sort(
+        key=lambda x: x[1].created_at or datetime.min.replace(tzinfo=timezone.utc),
+        reverse=True
+    )
+    items = items[:50]
+
+    for item_type, item in items:
         fe = fg.add_entry()
-        fe.id(f'https://9janetmovies.com.ng/movie/{movie.slug}')
-        fe.title(f'{movie.title} ({movie.year})' if movie.year else movie.title)
-        fe.link(href=f'https://9janetmovies.com.ng/movie/{movie.slug}')
-        fe.category({'term': movie.genre} if movie.genre else {'term': 'Movie'})
 
-        description = movie.description or f'Download {movie.title} free on 9janetmovies.'
-        if movie.poster_url:
-            description = f'<img src="{movie.poster_url}" alt="{movie.title}"/><br/>{description}'
+        if item_type == 'series':
+            url = f'https://9janetmovies.com.ng/series/{item.slug}'
+            title = f'{item.title} [Series]'
+        else:
+            url = f'https://9janetmovies.com.ng/movie/{item.slug}'
+            title = f'{item.title} ({item.year})' if item.year else item.title
+
+        fe.id(url)
+        fe.title(title)
+        fe.link(href=url)
+        fe.category({'term': item.genre} if item.genre else {'term': item_type.capitalize()})
+
+        description = item.description or f'Download {item.title} free on 9janetmovies.'
+        if item.poster_url:
+            description = f'<img src="{item.poster_url}" alt="{item.title}"/><br/>{description}'
         fe.description(description)
 
-        if movie.created_at:
-            fe.pubDate(movie.created_at.replace(tzinfo=timezone.utc))
+        if item.created_at:
+            fe.pubDate(item.created_at.replace(tzinfo=timezone.utc))
 
     response = make_response(fg.rss_str(pretty=True))
     response.headers['Content-Type'] = 'application/rss+xml; charset=utf-8'
